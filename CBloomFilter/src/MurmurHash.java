c8cc70e51d4d3bfc96ed17beeeaf65dd30bb3f04
Monorail <alexberliner@gmail.com>
2015-04-07 22:53:30 -0400

    added overflow structure

--- CBloomFilter/src/MurmurHash.java
+++ CBloomFilter/src/MurmurHash.java
@@ -1,16 +1,14 @@
-
 public class MurmurHash {
-	
-	public static int  hashItUp(Object key, int i){
-		
-		int hone= MurmurHash2(key, 42);
-		int htwo= MurmurHash2(key, 213);
-		
-		return hone+i*htwo;
+
+	public static int hashItUp(Object key, int i) {
+
+		int hone = MurmurHash2(key, 43);
+		int htwo = MurmurHash2(key, 213);
+
+		return hone + i * htwo;
 	}
-	
-	public static int MurmurHash2 (Object key, int seed )
-	{
+
+	public static int MurmurHash2(Object key, int seed) {
 		// 'm' and 'r' are mixing constants generated offline.
 		// They're not really 'magic', they just happen to work well.
 
@@ -22,14 +20,14 @@ public class MurmurHash {
 		int h = seed;
 		int k = key.hashCode();
 
-		k *= m; 
-		k ^= k >> r; 
-		k *= m; 
-			
-		h *= m; 
+		k *= m;
+		k ^= k >> r;
+		k *= m;
+
+		h *= m;
 		h ^= k;
 
 		return h;
 	}
-	
+
 }
